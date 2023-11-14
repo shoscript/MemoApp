dc6d55665658da3307a07b2521389d4f56eba265
shoscript <87895110+shoscript@users.noreply.github.com>
2023-11-14 16:58:15 +0900

    UI Components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,51 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+
+import Header from '../components/Header'
+import MemoListItem from '../components/MemoListItem'
+import CircleButton from '../components/CircleButton'
 
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
+      <Header />
       <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
-        </View>
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
       </View>
-      <View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-      </View>
-
-      <View>
-        <Text>+</Text>
-      </View>
+      <CircleButton>+</CircleButton>
     </View>
   )
 }
@@ -53,8 +21,7 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#ffffff'
   }
 })
 
